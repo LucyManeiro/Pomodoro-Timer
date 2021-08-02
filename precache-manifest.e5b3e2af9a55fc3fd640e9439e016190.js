e794e13799e8e2abd2bb77cc722d244e4869e843
Lucy Maneiro <lucybarnett.ot@gmail.com>
2021-08-01 19:53:23 -0500

    Updates

---
+++ precache-manifest.e5b3e2af9a55fc3fd640e9439e016190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce4c056b4f6c5f0e821581e20ede117",
+    "url": "/Pomodoro-Timer/index.html"
+  },
+  {
+    "revision": "3998861603dd0a9fa1e0",
+    "url": "/Pomodoro-Timer/static/css/main.fae812c3.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/Pomodoro-Timer/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pomodoro-Timer/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3998861603dd0a9fa1e0",
+    "url": "/Pomodoro-Timer/static/js/main.979b22dd.chunk.js"
+  },
+  {
+    "revision": "fa519a5d7be288e60c37",
+    "url": "/Pomodoro-Timer/static/js/runtime-main.3e9ec4f7.js"
+  }
+]);
